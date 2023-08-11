2b7f43fb2f76569889e5c6f45110f17fc78eb4b0
stanislavkononiuk <stanislavkononiuk@gmail.com>
2023-08-11 20:34:35 +0800

    preprocessing update

--- pymc_marketing/mmm/preprocessing.py
+++ pymc_marketing/mmm/preprocessing.py
@@ -65,8 +65,7 @@ class StandardizeControls:
     def standardize_control_data(self, data: pd.DataFrame) -> pd.DataFrame:
         control_data: pd.DataFrame = data[self.control_columns]
         transformers = [("scaler", StandardScaler())]
-        pipeline: Pipeline = Pipeline(steps=transformers)
-        self.control_transformer: Pipeline = pipeline.fit(X=control_data.to_numpy())
+        
         data[self.control_columns] = self.control_transformer.transform(
             control_data.to_numpy()
         )
